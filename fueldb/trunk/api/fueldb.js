d44f5d51e3249c59b7b5e769c53d82fa84d03863
unnamed38 <unnamed38@5e0b0ce5-4701-0010-9b8a-cb35bc870fdb>
2014-08-19 14:40:57 +0000

    Update authentication

--- fueldb/trunk/api/fueldb.js
+++ fueldb/trunk/api/fueldb.js
@@ -5,7 +5,7 @@
  */
 
 
-var FuelDB = function (uri, user, password, ssl) {
+var FuelDB = function (user, password, uri, ssl) {
 	'use strict';
 	var _uri;
 	var _ssl;
@@ -13,7 +13,6 @@ var FuelDB = function (uri, user, password, ssl) {
 	var _httpUri;
 	var _user = user;
 	var _password = password;
-	var _noAuthCmd = ["login"];
 	var _noTimeoutCmd = ["subscribe"];
 	var _subscribeEvent = {};
 	var _events = {};
@@ -30,10 +29,32 @@ var FuelDB = function (uri, user, password, ssl) {
 		_events[".INFO"] = function (e) {
 			console.log("[Info] " + e.value);
 		};
-		_events[".LOGIN_SUCC"] = function (e) {
-			_websocket.allowed = true;
-			_currentUser = e.value;
-			console.log("[Info] Login success as " + _currentUser + " !");
+	};
+
+	var _send = function (obj) {
+		if (_websocket.readyState === WebSocket.OPEN) {
+			_websocket.send(JSON.stringify(obj));
+		} else if (_websocket.readyState === WebSocket.CONNECTING && _noTimeoutCmd.indexOf(obj.type) === -1) {
+			setTimeout(function () {
+				_send(obj);
+			}, 200);
+		} else {
+			console.log("[Warning] Packet throwed away");
+			console.log(obj);
+		}
+	};
+	// Exposed function
+	this.connect = function (target) {
+		_uri = (target && target.uri) ? target.uri : "xxxxxxxx:xxxx";
+		_ssl = (target && target.ssl !== undefined) ? target.ssl : "yyyy";
+		_user = (target && target.user) ? target.user : _user;
+		_password = (target && target.password) ? target.password : _password;
+		_wsUri = "ws"+(_ssl?"s":"")+"://"+_uri;
+		_httpUri = "http"+(_ssl?"s":"")+"://"+_uri;
+
+		_websocket = new WebSocket(_wsUri+_computeURL());
+		_websocket.onopen = function () {
+			console.log("WebSocket opened successfully");
 			// Subscribe event initialization
 			for (var point in _subscribeEvent) {
 				for (var uid in _subscribeEvent[point]) {
@@ -41,54 +62,6 @@ var FuelDB = function (uri, user, password, ssl) {
 					_subscribe(point, _subscribeEvent[point][uid].cb, uid);
 				}
 			}
-			if (_events[".LOGIN_SUCC_2"]) {
-				_events[".LOGIN_SUCC_2"](e);
-			}
-		};
-		_events[".LOGIN_FAIL"] = function (e) {
-			_websocket.allowed = false;
-			console.log("[Error] Login failed !");
-			if (_events[".LOGIN_FAIL_2"]) {
-				_events[".LOGIN_FAIL_2"](e);
-			}
-		};
-		_events[".REQ_LOGIN"] = function () {
-			if (_currentUser && _user && _password) {
-				_login(_user,_password);
-			} else {
-				console.log("[Warning] You need to login !");
-			}
-		};
-	};
-
-	var _send = function (obj) {
-		if (_websocket.readyState === WebSocket.OPEN && (_websocket.allowed || _noAuthCmd.indexOf(obj.type) !== -1)) {
-			_websocket.send(JSON.stringify(obj));
-		} else if (_websocket.readyState === WebSocket.CONNECTING && _noTimeoutCmd.indexOf(obj.type) === -1 ||
-			(_websocket.readyState === WebSocket.OPEN && _websocket.allowed === undefined)) {
-			setTimeout(function () {
-				_send(obj);
-			}, 200);
-		} else if (_websocket.allowed !== undefined && !_websocket.allowed) {
-			console.log("[Warning] You need to login !");
-		} else if (_noTimeoutCmd.indexOf(obj.type) !== -1) {
-			// Subscription case
-			console.log("Subs packet throwed away");
-		} else {
-			console.log("[Warning] Packet throwed away");
-			console.log(obj);
-		}
-	};
-	// Exposed function
-	this.connect = function (uri, ssl) {
-		_uri = uri ? uri : "xxxxxxxx:xxxx";
-		_ssl = ssl === undefined ? "yyyy" : ssl;	
-		_wsUri = "ws"+(_ssl?"s":"")+"://"+_uri;
-		_httpUri = "http"+(_ssl?"s":"")+"://"+_uri;
-
-		_websocket = new WebSocket(_wsUri);
-		_websocket.onopen = function () {
-			console.log("WebSocket opened successfully");
 		};
 		_websocket.onmessage = function (evt) {
 			var obj = JSON.parse(evt.data);
@@ -105,11 +78,10 @@ var FuelDB = function (uri, user, password, ssl) {
 			}
 		};
 		_websocket.onerror = function (evt) {
-			console.log("Error: " + evt.data);
+			console.log("Error: " + evt.type);
 		};
-
 		_websocket.onclose = function (evt) {
-			console.log("WebSocket closed: " + evt.data);
+			console.log("WebSocket closed, code: " + evt.code+" reason: "+evt.reason);
 			setTimeout(function () {
 				//_init();
 				_connect();
@@ -117,24 +89,6 @@ var FuelDB = function (uri, user, password, ssl) {
 		};
 	};
 	var _connect = this.connect;
-
-	this.login = function (user, password, success, fail) {
-		_user = user ? user : _user;
-		_password = password ? password : _password;
-		if (success) {
-			_events[".LOGIN_SUCC_2"] = success;
-		}
-		if (fail) {
-			_events[".LOGIN_FAIL_2"] = fail;
-		}
-		var obj = {
-			"user" : user,
-			"password" : password,
-			"type" : "login"
-		};
-		_send(obj);
-	};
-	var _login = this.login;
 
 	this.disconnect = function () {
 		if (_websocket.readyState !== WebSocket.CLOSED){
@@ -247,12 +201,8 @@ var FuelDB = function (uri, user, password, ssl) {
 		_send(obj);
 	};
 
-	this.getCurrentUser = function () {
-		return _currentUser;
-	};
-
 	var _computeURL = function(point){
-		var toSign = "/"+point.split(".").join("/")+"?timestamp="+new Date().getTime()+"&user="+_user;
+		var toSign = "/"+(point ? point.split(".").join("/"):"")+"?timestamp="+new Date().getTime()+"&user="+_user;
 		var key = CryptoJS.HmacSHA256(_password,_user)+"";
 		var sign = CryptoJS.HmacSHA256(toSign,key);
 		return toSign+"&signature="+sign;
@@ -264,5 +214,5 @@ var FuelDB = function (uri, user, password, ssl) {
 	};
 
 	_init();
-	_connect(uri,ssl);
+	_connect({'uri':uri,'ssl':ssl});
 };
